b613ea07431116882af83f163da1dea99ee7203f
cox101 <104098884+cox101@users.noreply.github.com>
2024-10-28 21:23:58 +0300

    .git

--- Ecommerce-frontend/src/app.js
+++ Ecommerce-frontend/src/app.js
@@ -1,26 +1,42 @@
-// src/App.js
-import React from 'react';
-import Login from './components/Login';
-import Register from './components/Register';
-import ProductList from './components/ProductList';
-import Cart from './components/Cart';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import Login from "./components/Login";
+import Register from "./components/Register";
+//import "./styles.css"; // Importing global styles
 
 function App() {
   return (
     <Router>
       <div className="App">
+        {/* Navigation Bar */}
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <Link className="navbar-brand" to="/">E-commerce</Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item"><Link className="nav-link" to="/">Products</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/cart">Cart</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
-            </ul>
+          <div className="container">
+            <Link className="navbar-brand" to="/">E-Commerce</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Products</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/cart">Cart</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">Register</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
 
+        {/* Main Content */}
         <div className="container mt-4">
           <Routes>
             <Route path="/" element={<ProductList />} />
@@ -33,6 +49,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
