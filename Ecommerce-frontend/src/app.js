8f81edb1aef32c686fe77e00f546433aa5632234
cox101 <104098884+cox101@users.noreply.github.com>
2024-10-28 16:54:19 +0300

    Updates App.j

--- Ecommerce-frontend/src/app.js
+++ Ecommerce-frontend/src/app.js
@@ -7,14 +7,32 @@ import Cart from './components/Cart';
 
 function App() {
   return (
-    <div>
-      <h1>E-commerce Frontend</h1>
-      <Register />
-      <Login />
-      <ProductList />
-      <Cart />
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Link className="navbar-brand" to="/">E-commerce</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item"><Link className="nav-link" to="/">Products</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/cart">Cart</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
