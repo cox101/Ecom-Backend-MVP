31cc4f8fd283099dc40d97fb7e1afe705ca761b0
cox101 <104098884+cox101@users.noreply.github.com>
2024-10-28 16:56:35 +0300

    Update register.js

--- Ecommerce-frontend/src/components/Register.js
+++ Ecommerce-frontend/src/components/Register.js
@@ -45,4 +45,23 @@ const Register = () => {
   );
 };
 
+function Register() {
+  return (
+    <div className="col-md-6 offset-md-3">
+      <h2>Register</h2>
+      <form>
+        <div className="form-group">
+          <label>Email address</label>
+          <input type="email" className="form-control" placeholder="Enter email" />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" className="form-control" placeholder="Password" />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3">Register</button>
+      </form>
+    </div>
+  );
+}
+
 export default Register;
