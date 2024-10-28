c01face6819088b9b0a023392ab94d0ff50368ac
cox101 <104098884+cox101@users.noreply.github.com>
2024-10-28 16:55:55 +0300

    Update login.js

--- Ecommerce-frontend/src/components/Login.js
+++ Ecommerce-frontend/src/components/Login.js
@@ -38,4 +38,23 @@ const Login = () => {
   );
 };
 
+function Login() {
+  return (
+    <div className="col-md-6 offset-md-3">
+      <h2>Login</h2>
+      <form>
+        <div className="form-group">
+          <label>Email address</label>
+          <input type="email" className="form-control" placeholder="Enter email" />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" className="form-control" placeholder="Password" />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3">Login</button>
+      </form>
+    </div>
+  );
+}
+
 export default Login;
